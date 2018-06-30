07d3da37a51dd84a288b9fd71eece0d26dcfc1c0
Roberto Rocha <rarp12@gmail.com>
2018-06-30 11:33:04 -0500

    Comit api

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res, next) => {
+  res.json({
+    message: 'Welcome to API',
+  });
+});
+
+app.use((req, res, next) => {
+  res.status(404);
+  res.json({
+    message: 'Not found',
+  });
+});
+
+app.use((err, req, res, next) => {
+  res.status(500);
+  res.json({
+    message: err.message,
+  });
+});
+
+module.exports = app;
