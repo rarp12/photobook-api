8d79aaaba105c417f4381d19d5ca0b8c3452b764
Roberto Rocha <rarp12@gmail.com>
2018-07-06 21:02:32 -0500

    delete update

--- server/api/v1/posts/controller.js
+++ server/api/v1/posts/controller.js
@@ -1,45 +1,82 @@
+const Model = require('./model');
+
+exports.id = (req, res, next, id) => {
+  Model.findById(id)
+    .then((doc) => {
+      if (doc) {
+        req.doc = doc;
+        next();
+      } else {
+        res.json({
+          message: `${Model.modelName} not found`,
+        });
+      }
+    })
+    .catch((err) => {
+      next(new Error(err));
+    });
+};
+
 exports.all = (req, res, next) => {
-  res.json([]);
+  Model.find()
+    .then((docs) => {
+      res.json(docs);
+    })
+    .catch((err) => {
+      next(new Error(err));
+    });
 };
 
 exports.create = (req, res, next) => {
   const {
     body,
   } = req;
-  res.json({
-    item: body,
-  });
+
+  const document = new Model(body);
+  document.save()
+    .then((doc) => {
+      res.json(doc);
+    })
+    .catch((err) => {
+      next(new Error(err));
+    });
 };
 
 exports.read = (req, res, next) => {
   const {
-    params,
+    doc,
   } = req;
-  res.json({
-    item: {
-      id: params.id,
-    },
-  });
+
+  res.json(doc);
 };
 
 exports.update = (req, res, next) => {
   const {
-    params,
+    doc,
+    body,
   } = req;
-  res.json({
-    item: {
-      id: params.id,
-    },
-  });
+
+  Object.assign(doc, body);
+
+  doc.save()
+    .then((updated) => {
+      res.json(updated);
+    })
+    .catch((err) => {
+      next(new Error(err));
+    });
 };
 
 exports.delete = (req, res, next) => {
   const {
-    params,
+    doc,
   } = req;
-  res.json({
-    item: {
-      id: params.id,
-    },
-  });
+
+  doc.remove()
+    .then((deleted) => {
+      res.json(deleted);
+    })
+    .catch((err) => {
+      next(new Error(err));
+    });
 };
